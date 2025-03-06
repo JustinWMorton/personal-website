8d7346edddb3d898f122955ddb273ec5d64124dd
JustinWMorton <squishymort@gmail.com>
2025-03-05 22:46:33 -0700

    vercel

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['your-domain.com'],
     formats: ["image/webp"],
